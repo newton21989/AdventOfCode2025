2211b688c5da3460d7239ea7f7a2c7404a1fc57a
Q-tip <newton21989@gmail.com>
2025-12-14 22:33:08 -0600

    Day 2 part 2 complete

--- day02.php
+++ day02.php
@@ -3,23 +3,41 @@
 $file = file_get_contents('./data/day02.txt');
 $ranges = explode(',', $file);
 
-$out1 = 0;
+$out2 = 0;
 
 foreach($ranges as $range) {
   $bounds = explode('-', $range);
-  for($i = $bounds[0]; $i <= $bounds[1]; $i++) {
-    if(strlen($i) % 2 != 0) {
-      continue;
+  for($i = $bounds[0]; $i <= $bounds[1]; $i++) { //ID to test
+    $isInvalid = false;  //assume the ID is valid to start
+    for($j = 1; $j <= strlen($i)/2; $j++) {  //loop through chunk sizes, increasing progressively
+      if(strlen($i) % $j != 0) { //ignore invalid chunk size
+        continue;
+      }
+
+      $p = str_split($i, $j); //split ID into chuncks of size $j
+      $match = true; //assume chunk matches pattern
+      foreach($p as $k=>$q) {
+        if($k == 0) {  //first chunk is the one we're comparing against, so it has nothing to compare to on first pass
+          continue;
+        }
+        if($q != $p[0]) {
+          $match = false; //if any chunk fails, stop checking
+          break;
+        }
+      }
+
+      if($match) {
+        $isInvalid = true; //if we made it this far and all chunks matched, the ID is invalid
+        break;
+      }
+
     }
-
-    $p1 = substr($i, 0, strlen($i)/2);
-    $p2 = substr($i, strlen($i)/2);
-
-    if($p1 == $p2) {
+    if($isInvalid) {
       echo($i . "\n");
-      $out1 += $i;
+      $out2 += $i;
+      continue;
     }
   }
 }
 
-echo("Day 2 part 1: $out1\n");
+echo("Day 2 part 2: $out2\n");
