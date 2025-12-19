1abfd929866c11ce9f951434e85a052bf0799f92
Q-tip <newton21989@gmail.com>
2025-12-18 22:37:21 -0600

    Day 3 part 2 complete

--- day03.php
+++ day03.php
@@ -33,8 +33,36 @@ foreach($batteries as $batt) {
 
     $out = $max2 . $max1;
   }
-  echo("$out\n");
+  //echo("$out\n");
   $out1 += intval($out);
 }
 
 echo("Day 3 part 1: $out1\n");
+
+$out2 = 0;
+const DIGITS = 12;
+
+foreach($batteries as $batt) {
+  if($batt == "") {
+    continue;
+  }
+
+  $cells = str_split($batt);
+  $numcells = count($cells);
+  $selected = [];
+  $winstart = 0;
+  $winlen = $numcells - DIGITS + 1;
+
+  for($i = 0; $i < DIGITS; $i++) {
+    $cells2 = $cells;
+    $window = array_slice($cells2, $winstart, $winlen);
+    $selected[$i] = array_max($window);
+    $winstart = array_search($selected[$i], $window) + $winstart + 1;
+    $winlen -= array_search($selected[$i], $window);
+  }
+
+  //echo(implode('', $selected) . "\n");
+  $out2 += intval(implode('', $selected));
+}
+
+echo("Day 3 part 2: $out2\n");
