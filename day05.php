e998b16504dee1324e8c28d0ee41b9774a46ff53
Q-tip <newton21989@gmail.com>
2025-12-20 21:24:04 -0600

    Day 5 part 2 pass test (sqlite)

--- day05.php
+++ day05.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = file_get_contents('./data/day05.txt');
+$file = file_get_contents('./test_data/day05.txt');
 $split1 = explode("\n\n", $file);
 
 $dbstr = explode("\n", $split1[0]);
@@ -28,14 +28,36 @@ foreach($ingredients as $ingredient) {
 
 echo("Day 5 part 1: $numfresh\n");
 
-$db2 = [];
+$dbpath = './test_data/day05.db';
+
+if(file_exists($dbpath)) {
+  unlink($dbpath);
+}
+
+$db2 = new SQLite3($dbpath);
+$result = $db2->query("CREATE TABLE fresh_ingredients(id INTEGER)");
+if(!$result) {
+  die("Problem with SQL: " . $db2->lastErrorMsg() . "\n");
+}
+
 foreach($db as $range) {
   for($i = $range[0]; $i <= $range[1]; $i++) {
-    if(array_search($i, $db2) === false) {
-      array_push($db2, $i);
+    $result = $db2->query("INSERT INTO fresh_ingredients ('id') VALUES ($i)");
+    if(!$result) {
+      die("Problem with SQL: " . $db2->lastErrorMsg() . "\n");
     }
   }
 }
 
-$out2 = count($db2);
-echo("Day 5 part 2: $out2\n");
+$result = $db2->query("SELECT DISTINCT(id) FROM fresh_ingredients");
+if($result) {
+  $resultsarr = [];
+  while($j = $result->fetchArray(SQLITE3_NUM)) {
+    array_push($resultsarr, $j[0]);
+  }
+  $out2 = count($resultsarr);
+  echo("Day 5 part 2: $out2\n");
+}
+else {
+  die("Problem with SQL query: " . $db2->lastErrorMsg() . "\n");
+}
