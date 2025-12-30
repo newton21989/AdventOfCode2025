8afb803f84a807240aa71b69c60f5c3c864bf8c0
Q-tip <newton21989@gmail.com>
2025-12-29 22:18:11 -0600

    Day 10 part 2 pass test

--- day10.php
+++ day10.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = file_get_contents('./data/day10.txt');
+$file = file_get_contents('./test_data/day10.txt');
 $machines = explode("\n", $file);
 
 function btnCombos($buttons, $length, $start = 0, $chosen = []): Generator {
@@ -56,3 +56,51 @@ foreach($machines as $m) {
 }
 
 echo("Day 10 part 1: $out1\n");
+
+$out2 = 0;
+foreach($machines as $m) {
+  $a = $b = [];
+  preg_match("/\{(.*)\}/", $m, $a);
+  $target = explode(",", $a[1]);
+
+  preg_match_all("/\(([\d,]+)\)/", $m, $b);
+  $buttons = $b[1];
+  $buttons = array_map(
+    fn($b) => explode(",", $b),
+    $buttons
+  );
+
+  $queue = [];
+  $visited = [];
+  $test = array_fill(0, count($target), 0);
+  foreach($buttons as $b) {
+    $cur = $test;
+    foreach($b as $c) {
+      $cur[$c] += 1;
+    }
+    $queue[] = ['state'=>$cur, 'level'=>1];
+    $visited[implode(",", $cur)] = true;
+  }
+
+  while($start = array_shift($queue)) {
+    foreach($buttons as $b) {
+      $cur = $start['state'];
+      $level = $start['level'] + 1;
+      foreach($b as $c) {
+        $cur[$c] += 1;
+      }
+
+      if(!isset($visited[implode(",", $cur)])) {
+        if($cur == $target) {
+          $out2 += $level;
+          break 2;
+        }
+
+        $queue[] = ['state'=>$cur, 'level'=>$level];
+        $visited[implode(",", $cur)] = true;
+      }
+    }
+  }
+}
+
+echo("Day 10 part 2: $out2\n");
